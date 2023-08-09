27bcd1e7ff6f3394e54d1411499cb2408f00e220
Jonathan Carse <jonathan@vhive.ai>
2023-08-09 14:22:14 +0300

    added all routes

--- server/index.js
+++ server/index.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const articles = [];
+
 // set express to use JSON as body format
 app.use(express.json())
+
+// add CORS headers for debugging to the express server
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 // create a GET route with a path variable called articleId
 // also add a query parameter called question
@@ -17,19 +25,46 @@ app.get('/article/:articleId/answer', (req, res) => {
 });
 
 // create a POST route called /article/:articleId/question which accepts a payload that has question and answer properties
+// the articleId should be a path variable
+// the article should be found in the articles array and the question and answer should be added to the qanda array
 app.post('/article/:articleId/question', (req, res) => {
   // get the articleId path variable
   const articleId = req.params.articleId
-  // get the question and answer properties from the request body
+  // get the question and answer from the request body
   const { question, answer } = req.body
-  // send back the articleId, question, and answer
-  res.send(`articleId: ${articleId}, question: ${question}, answer: ${answer}`)
+
+  // find the article in the articles array
+  const article = articles.find(article => article.id === articleId)
+
+  // add the question and answer to the article
+  article.qanda.push({ question, answer })
+
+  // send back the question and answer
+  res.send(`question: ${question}, answer: ${answer}`)
 });
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
+
+// create a POST route for a single article. the payload should have the fields name and context
+app.post('/article', (req, res) => {
+  // get the name and context fields from the request body
+  const { name, context, id } = req.body
+
+  // add the article to the articles array
+  articles.push({ name, context, id, qanda: [] });
+
+  // send back the name and context
+  res.send(`name: ${name}, context: ${context}`)
+});
+
+// create a GET route for all articles
+app.get('/article', (req, res) => {
+  // send back the articles
+  res.send(articles)
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
